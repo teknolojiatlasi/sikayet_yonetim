4b2dacb7ad184f1aae3e99b70256c6a0a3ddf2e1
beshir <javahesaphesap@gmail.com>
2026-04-04 16:07:08 +0300

    ilk hali

---
+++ sikayet/src/main/java/com/example/sikayet/SikayetApplication.java
@@ -0,0 +1,13 @@
+package com.example.sikayet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SikayetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SikayetApplication.class, args);
+	}
+
+}
